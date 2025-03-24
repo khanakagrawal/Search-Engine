05c11b528deae27e2cf8813dac75dbb95652702c
Khanak Agrawal <khanak@Khanaks-MacBook-Air.local>
2025-03-23 23:52:27 -0700

    main files for setting up and starting server and helper functions

---
+++ HttpUtils.cc
@@ -0,0 +1,320 @@
+/*
+ * Copyright ©2024 Hannah C. Tang.  All rights reserved.  Permission is
+ * hereby granted to students registered for University of Washington
+ * CSE 333 for use solely during Autumn Quarter 2024 for purposes of
+ * the course.  No other use, copying, distribution, or modification
+ * is permitted without prior written consent. Copyrights for
+ * third-party components of this work must be honored.  Instructors
+ * interested in reusing these course materials should contact the
+ * author.
+ */
+
+// This file contains a number of HTTP and HTML parsing routines
+// that come in useful throughput the assignment.
+
+#include "./HttpUtils.h"
+
+#include <arpa/inet.h>
+#include <errno.h>
+#include <limits.h>
+#include <netdb.h>
+#include <boost/algorithm/string/replace.hpp>
+#include <boost/algorithm/string.hpp>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <cstdint>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <utility>
+
+using boost::algorithm::replace_all;
+using std::cerr;
+using std::endl;
+using std::map;
+using std::pair;
+using std::string;
+using std::vector;
+
+namespace hw4 {
+
+bool IsPathSafe(const string &root_dir, const string &test_file) {
+  // rootdir is a directory path. testfile is a path to a file.
+  // return whether or not testfile is within rootdir.
+  // Be sure that your code handles the case when"." and ".."
+  // are found in the path for testfile.
+  //
+  // (HINT: It can be tricky to handle all of the cases where "."
+  // and ".." show up in the passed in file paths. It would be easier
+  // to just handle the absolute paths for the files (no "." or "..").
+  // You may want to see if there is a C function that gets the absolute
+  // path of a file.)
+
+  // STEP 1
+  if (root_dir.empty() || test_file.empty()) {
+    return false;
+  }
+
+  char file_path[PATH_MAX];
+  char dir_path[PATH_MAX];
+
+  // get the absolute path for the directory and the file
+  if (realpath(root_dir.c_str(), dir_path) == NULL ||
+      realpath(test_file.c_str(), file_path) == NULL) {
+    return false;
+  }
+
+  size_t dir_path_len = strlen(dir_path);
+
+  // make sure the length of the file path is more than that of the directory
+  if (dir_path_len > strlen(file_path)) {
+    return false;
+  }
+
+  // seperate the dir path and file name from the file path
+  string ck_file_path = string(file_path).substr(0, dir_path_len);
+  string ck_file_name = string(file_path).substr(dir_path_len,
+                                                 strlen(file_path));
+
+  // make sure the dir path from file path is the same as the dir path
+  if (ck_file_path != string(dir_path)) {
+    return false;
+  }
+
+  // make sure the char after dir path is / for the next part or is the end
+  if (file_path[dir_path_len] != '/' && file_path[dir_path_len] != '\0') {
+    return false;
+  }
+
+  return true;  // you may want to change this return value
+}
+
+string EscapeHtml(const string &from) {
+  string ret = from;
+  // Read through the passed in string, and replace any unsafe
+  // html tokens with the proper escape codes. The characters
+  // that need to be escaped in HTML are the same five as those
+  // that need to be escaped for XML documents. You can see an
+  // example in the comment for this function in HttpUtils.h and
+  // the rest of the characters that need to be replaced can be
+  // looked up online.
+
+  // STEP 2
+  for (uint32_t i = 0; i < ret.length(); i++) {
+    if (ret[i] == '"') {
+      ret.replace(i, 1, "&quot;");
+      continue;
+    } else if (ret[i] == '\'') {
+      ret.replace(i, 1, "&apos;");
+      continue;
+    } else if (ret[i] == '<') {
+      ret.replace(i, 1, "&lt;");
+      continue;
+    } else if (ret[i] == '>') {
+      ret.replace(i, 1, "&gt;");
+      continue;
+    } else if (ret[i] == '&') {
+      // make sure this & isn't the start of an escape code
+      string sub_long = ret.substr(i+1, 5);
+      string sub_short = ret.substr(i+1, 3);
+      if (sub_long == "quot;" || sub_long == "apos;" ||
+          sub_short == "lt;" || sub_short == "gt;") {
+        continue;
+      }
+
+      // if not, replace it with & escape code
+      ret.replace(i, 1, "&amp;");
+    }
+  }
+  return ret;
+}
+
+// Look for a "%XY" token in the string, where XY is a
+// hex number.  Replace the token with the appropriate ASCII
+// character, but only if 32 <= dec(XY) <= 127.
+string URIDecode(const string &from) {
+  string retstr;
+
+  // Loop through the characters in the string.
+  for (unsigned int pos = 0; pos < from.length(); pos++) {
+    // note: use pos+n<from.length() instead of pos<from.length-n
+    // to avoid overflow problems with unsigned int values
+    char c1 = from[pos];
+    char c2 = (pos+1 < from.length()) ? toupper(from[pos+1]) : ' ';
+    char c3 = (pos+2 < from.length()) ? toupper(from[pos+2]) : ' ';
+
+    // Special case the '+' for old encoders.
+    if (c1 == '+') {
+      retstr.append(1, ' ');
+      continue;
+    }
+
+    // Is this an escape sequence?
+    if (c1 != '%') {
+      retstr.append(1, c1);
+      continue;
+    }
+
+    // Yes.  Are the next two characters hex digits?
+    if (!((('0' <= c2) && (c2 <= '9')) ||
+          (('A' <= c2) && (c2 <= 'F')))) {
+      retstr.append(1, c1);
+      continue;
+    }
+    if (!((('0' <= c3) && (c3 <= '9')) ||
+           (('A' <= c3) && (c3 <= 'F')))) {
+      retstr.append(1, c1);
+      continue;
+    }
+
+    // Yes.  Convert to a code.
+    uint8_t code = 0;
+    if (c2 >= 'A') {
+      code = 16 * (10 + (c2 - 'A'));
+    } else {
+      code = 16 * (c2 - '0');
+    }
+    if (c3 >= 'A') {
+      code += 10 + (c3 - 'A');
+    } else {
+      code += (c3 - '0');
+    }
+
+    // Is the code reasonable?
+    if (!((code >= 32) && (code <= 127))) {
+      retstr.append(1, c1);
+      continue;
+    }
+
+    // Great!  Convert and append.
+    retstr.append(1, static_cast<char>(code));
+    pos += 2;
+  }
+  return retstr;
+}
+
+void URLParser::Parse(const string &url) {
+  url_ = url;
+
+  // Split the URL into the path and the args components.
+  vector<string> ps;
+  boost::split(ps, url, boost::is_any_of("?"));
+  if (ps.size() < 1)
+    return;
+
+  // Store the URI-decoded path.
+  path_ = URIDecode(ps[0]);
+
+  if (ps.size() < 2)
+    return;
+
+  // Split the args into each field=val; chunk.
+  vector<string> vals;
+  boost::split(vals, ps[1], boost::is_any_of("&"));
+
+  // Iterate through the chunks.
+  for (unsigned int i = 0; i < vals.size(); i++) {
+    // Split the chunk into field, value.
+    string val = vals[i];
+    vector<string> fv;
+    boost::split(fv, val, boost::is_any_of("="));
+    if (fv.size() == 2) {
+      // Add the field, value to the args_ map.
+      args_[URIDecode(fv[0])] = URIDecode(fv[1]);
+    }
+  }
+}
+
+uint16_t GetRandPort() {
+  uint16_t portnum = 10000;
+  portnum += ((uint16_t) getpid()) % 25000;
+  portnum += ((uint16_t) rand()) % 5000;  // NOLINT(runtime/threadsafe_fn)
+  return portnum;
+}
+
+int WrappedRead(int fd, unsigned char *buf, int read_len) {
+  int res;
+  while (1) {
+    res = read(fd, buf, read_len);
+    if (res == -1) {
+      if ((errno == EAGAIN) || (errno == EINTR))
+        continue;
+    }
+    break;
+  }
+  return res;
+}
+
+int WrappedWrite(int fd, const unsigned char *buf, int write_len) {
+  int res, written_so_far = 0;
+
+  while (written_so_far < write_len) {
+    res = write(fd, buf + written_so_far, write_len - written_so_far);
+    if (res == -1) {
+      if ((errno == EAGAIN) || (errno == EINTR))
+        continue;
+      break;
+    }
+    if (res == 0)
+      break;
+    written_so_far += res;
+  }
+  return written_so_far;
+}
+
+bool ConnectToServer(const string &host_name, uint16_t port_num,
+                     int *client_fd) {
+  struct addrinfo hints;
+  struct addrinfo *results;
+  struct addrinfo *r;
+  int client_sock, ret_val;
+  char port_str[10];
+
+  // Convert the port number to a C-style string.
+  snprintf(port_str, sizeof(port_str), "%hu", port_num);
+
+  // Zero out the hints data structure using memset.
+  memset(&hints, 0, sizeof(hints));
+
+  // Indicate we're happy with either AF_INET or AF_INET6 addresses.
+  hints.ai_family = AF_UNSPEC;
+
+  // Constrain the answers to SOCK_STREAM addresses.
+  hints.ai_socktype = SOCK_STREAM;
+
+  // Do the lookup.
+  if ((ret_val = getaddrinfo(host_name.c_str(),
+                            port_str,
+                            &hints,
+                            &results)) != 0) {
+    cerr << "getaddrinfo failed: ";
+    cerr << gai_strerror(ret_val) << endl;
+    return false;
+  }
+
+  // Loop through, trying each out until one succeeds.
+  for (r = results; r != nullptr; r = r->ai_next) {
+    // Try manufacturing the socket.
+    if ((client_sock = socket(r->ai_family, SOCK_STREAM, 0)) == -1) {
+      continue;
+    }
+    // Try connecting to the peer.
+    if (connect(client_sock, r->ai_addr, r->ai_addrlen) == -1) {
+      continue;
+    }
+    *client_fd = client_sock;
+    freeaddrinfo(results);
+    return true;
+  }
+  freeaddrinfo(results);
+  return false;
+}
+
+}  // namespace hw4
---
+++ http333d.cc
@@ -0,0 +1,146 @@
+/*
+ * Copyright ©2024 Hannah C. Tang.  All rights reserved.  Permission is
+ * hereby granted to students registered for University of Washington
+ * CSE 333 for use solely during Autumn Quarter 2024 for purposes of
+ * the course.  No other use, copying, distribution, or modification
+ * is permitted without prior written consent. Copyrights for
+ * third-party components of this work must be honored.  Instructors
+ * interested in reusing these course materials should contact the
+ * author.
+ */
+
+#include <dirent.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <signal.h>
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <list>
+#include <filesystem>
+#include <fstream>
+
+#include "./ServerSocket.h"
+#include "./HttpServer.h"
+
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::list;
+using std::string;
+
+// min port number to check for
+static const int kPortNum = 1024;
+
+// Print out program usage, and exit() with EXIT_FAILURE.
+static void Usage(char *prog_name);
+
+// Parse command-line arguments to get port, path, and indices to use
+// for your http333d server.
+//
+// Params:
+// - argc: number of argumnets
+// - argv: array of arguments
+// - port: output parameter returning the port number to listen on
+// - path: output parameter returning the directory with our static files
+// - indices: output parameter returning the list of index file names
+//
+// Calls Usage() on failure. Possible errors include:
+// - path is not a readable directory
+// - index file names are readable
+static void GetPortAndPath(int argc,
+                           char **argv,
+                           uint16_t *const port,
+                           string *const path,
+                           list<string> *const indices);
+
+int main(int argc, char **argv) {
+  // Print out welcome message.
+  cout << "Welcome to http333d, the UW CSE333 web server!" << endl;
+  cout << "  Copyright 2012 Steven Gribble" << endl;
+  cout << "  http://www.cs.washington.edu/homes/gribble" << endl;
+  cout << endl;
+  cout << "initializing:" << endl;
+  cout << "  parsing port number and static files directory..." << endl;
+
+  // Ignore the SIGPIPE signal, otherwise we'll crash out if a client
+  // disconnects unexpectedly.
+  signal(SIGPIPE, SIG_IGN);
+
+  // Get the port number and list of index files.
+  uint16_t port_num;
+  string static_dir;
+  list<string> indices;
+  GetPortAndPath(argc, argv, &port_num, &static_dir, &indices);
+  cout << "    port: " << port_num << endl;
+  cout << "    static file path: " << static_dir << endl;
+
+  // Run the server.
+  hw4::HttpServer hs(port_num, static_dir, indices);
+  if (!hs.Run()) {
+    cerr << "  server failed to run!?" << endl;
+  }
+
+  cout << "server completed!  Exiting." << endl;
+  return EXIT_SUCCESS;
+}
+
+
+static void Usage(char *prog_name) {
+  cerr << "Usage: " << prog_name << " port staticfiles_directory indices+";
+  cerr << endl;
+  exit(EXIT_FAILURE);
+}
+
+static void GetPortAndPath(int argc,
+                           char **argv,
+                           uint16_t *const port,
+                           string *const path,
+                           list<string> *const indices) {
+  // Here are some considerations when implementing this function:
+  // - There is a reasonable number of command line arguments
+  // - The port number is reasonable
+  // - The path (i.e., argv[2]) is a readable directory
+  // - You have at least 1 index, and all indices are readable file
+
+  // STEP 1:
+
+  // check for reasonable number of args
+  // executable name, port, path, and at least 1 index
+  if (argc < 4) {
+    Usage(argv[1]);
+  }
+
+  // check if port is a reasonable number
+  if (std::stoi(argv[1]) < kPortNum) {
+    cerr << "Port should be >= 1024" << endl;
+    Usage(argv[0]);
+  }
+  *port = std::stoi(argv[1]);
+
+  // check directory path is an actual readable directory
+  if (!std::filesystem::is_directory(argv[2]) ||
+      !std::filesystem::exists(argv[2])) {
+    cerr << "Not a valid readable directory" << endl;
+    Usage(argv[0]);
+  }
+  *path = argv[2];
+
+  // go through remaining args to check for valid indices
+  for (int i = 3; i < argc; i++) {
+    std::ifstream file(argv[i], std::fstream::in);
+
+    // check if the file exists and is readable
+    if (!std::filesystem::exists(argv[i]) || file.bad()) {
+      cerr << "index " << std::to_string(i - 3)
+          << " is not a valid index" << endl;
+
+      Usage(argv[0]);
+    }
+
+    indices->push_back(argv[i]);
+  }
+}
+
